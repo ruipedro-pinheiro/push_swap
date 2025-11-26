e734a927f33045aad87db7eb3d74a759ec1c701e
pedropmelojs <pedroyverdon@gmail.com>
2025-11-26 17:35:35 +0100

    added basic empty files.

--- src/push_swap_operations.c
+++ src/push_swap_operations.c
@@ -1,30 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   push_swap_operations.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpinheir <rpinheir@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/26 14:33:05 by rpinheir          #+#    #+#             */
-/*   Updated: 2025/11/26 17:11:30 by rpinheir         ###   ########.fr       */
+/*   Created: 2025/11/26 17:26:35 by rpinheir          #+#    #+#             */
+/*   Updated: 2025/11/26 17:26:56 by rpinheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "../include/push_swap.h"
 
-# include "../libft/libft.h"
-# include <stdbool.h> 
-
-typedef struct stack
-{
-	int				value;
-	int				push_price;
-	int				position;
-	bool			cheapest;
-	bool			above_median;
-	struct stack	next;
-	struct stack	prev;
-}					t_stack;
-
-#endif

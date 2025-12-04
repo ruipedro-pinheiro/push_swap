c81c3094652984f884c9b8e6bf8b70feccbde348
pedropmelojs <pedroyverdon@gmail.com>
2025-12-04 18:36:27 +0100

    a

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,29 +6,48 @@
 /*   By: rpinheir <rpinheir@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 14:59:18 by rpinheir          #+#    #+#             */
-/*   Updated: 2025/11/27 16:52:23 by rpinheir         ###   ########.fr       */
+/*   Updated: 2025/12/04 18:02:23 by rpinheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 #include "../libft/libft.h"
 
-int	main(int argc, char **argv)
+/*
+** @brief Lit le parametre `argv` depuis le main et le formatte en int
+** @param argc  Compte combien de arguments sont donnes,
+** @param argv  Tableau 2D de tous les arguments donnes
+** @note `argc` compte en veriter les arguments depuis 1,
+** 0 etant le nom du programme.
+*/
+int	arg_parser(int argc, char **argv)
 {
 	int	i;
 	int	j;
 
-	i = 0;
+	i = 1;
 	j = 0;
-	while (i <= argc && **argv)
+	while (i < argc && argv[i][j] && *argv[i])
 	{
-		while (argv[i][j])
+		while (argv[i][j] && *argv[i])
 		{
-			ft_printf("%c", argv[i][j]);
+			ft_printf("%c\n", argv[i][j]);
 			j++;
 		}
 		j = 0;
 		i++;
 	}
-    return (1);
+}
+
+/*
+** @brief Fonction principale qui appelle le reste des fonctions necessaires
+** @param argc  Compte combien de arguments sont donnes
+** @param argv  Tableau 2D de tous les arguments donnes
+** @note `argc` compte en veriter les arguments depuis 1,
+** 0 etant le nom du programme.
+*/
+int	main(int argc, char **argv)
+{
+	ft_printf("Arguments = %i \n", argc - 1);
+	arg_parser(argc, **argv);
 }

47c86b06e9d7f90048bddfc61cc435264a993286
pedropmelojs <pedroyverdon@gmail.com>
2025-12-05 16:42:24 +0100

    Started doing the parser

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,12 +6,13 @@
 /*   By: rpinheir <rpinheir@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 14:59:18 by rpinheir          #+#    #+#             */
-/*   Updated: 2025/12/05 13:45:54 by rpinheir         ###   ########.fr       */
+/*   Updated: 2025/12/05 15:49:48 by rpinheir         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "../include/push_swap.h"
 #include "../libft/libft.h"
+#include <limits.h>
 
 /*
 ** @brief Parse les arguments de la ligne de commande
@@ -21,28 +22,44 @@
 ** @note `argc` inclut le nom du programme (argv[0]),
 ** donc argc - 1 = nombre d'arguments réels
 */
-int	arg_parser(int argc, char **argv)
+int	arg_parser(char **argv)
 {
-	int		i;
-	int		j;
-	char	*input;
+	int			i;
+	int			x;
+	int			y;
+	long long	output;
+	char		**input;
 
 	i = 0;
-	j = 0;
-	input = '\0';
-	if (ft_isdigit(argv[i][j]) == 0)
+	x = 0;
+	y = 0;
+	output = 0;
+	while (argv[1][i])
+	// 1 ici = le premier argument réellement pas le nom du programme
 	{
-		input = ft_split(argv[i], " ");
-		if (!input)
-			free(input);
+		if (ft_isdigit(argv[1][i]) == 0)
+		{
+			input = ft_split(argv[1], ' ');
+			if (!input)
+				free(input);
+			while (input[x][y])
+			{
+				output = ft_atoll(input[x]);
+				x++;
+			}
+			if (output < INT_MIN || output > INT_MAX)
+				ft_putendl_fd("Error", 1);
+		}
+		else
+		{
+			/* TODO: on input les int SANS parser de ascii en int
+			**	Cela n'est pas nécessaire, vu qu'on *SAIT*
+			**	que c'est des int et pas du ascii
+			*/
+		}
+		i++;
 	}
-	else
-	{
-		/* TODO: on input les int SANS parser de ascii en int
-		**	Cela n'est pas nécessaire, vu qu'on *SAIT*
-		**	que c'est des int et pas du ascii
-		*/
-	}
+	return (output);
 }
 
 /*
@@ -56,6 +73,8 @@ int	arg_parser(int argc, char **argv)
 */
 int	main(int argc, char **argv)
 {
-	ft_printf("Arguments = %i \n", argc - 1);
-	arg_parser(argc, argv);
+	if (argc > 1)
+	{
+		ft_printf("Output %i \n", arg_parser(argv));
+	}
 }

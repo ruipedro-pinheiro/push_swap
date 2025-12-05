18b8124c5b6887c2a1caba9084f2dbc927286f6d
pedropmelojs <pedroyverdon@gmail.com>
2025-12-05 13:50:15 +0100

    Subtle changes in indentations

--- src/push_swap.c
+++ src/push_swap.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
@@ -6,48 +6,56 @@
 /*   By: rpinheir <rpinheir@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 14:59:18 by rpinheir          #+#    #+#             */
-/*   Updated: 2025/12/04 19:49:24 by rpinheir         ###   ########.fr       */
+/*   Updated: 2025/12/05 13:45:54 by rpinheir         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
+
+#include "../include/push_swap.h"
+#include "../libft/libft.h"
 
 /*
 ** @brief Parse les arguments de la ligne de commande
 ** @param argc Nombre d'arguments passes au programme
-** @param argv Tableau de chaines contenant les arguments
-** @return int (a implementer: code de retour)
+** @param argv Tableau de chaînes contenant les arguments
+** @return int (a implémenter: code de retour)
 ** @note `argc` inclut le nom du programme (argv[0]),
-** donc argc - 1 = nombre d'arguments reels
+** donc argc - 1 = nombre d'arguments réels
 */
 int	arg_parser(int argc, char **argv)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	char	*input;
 
-	i = 1;
+	i = 0;
 	j = 0;
-	while (i < argc && argv[i][j] && *argv[i])
+	input = '\0';
+	if (ft_isdigit(argv[i][j]) == 0)
 	{
-		while (argv[i][j] && *argv[i])
-		{
-			ft_printf("%c\n", argv[i][j]);
-			j++;
-		}
-		j = 0;
-		i++;
+		input = ft_split(argv[i], " ");
+		if (!input)
+			free(input);
+	}
+	else
+	{
+		/* TODO: on input les int SANS parser de ascii en int
+		**	Cela n'est pas nécessaire, vu qu'on *SAIT*
+		**	que c'est des int et pas du ascii
+		*/
 	}
 }
 
 /*
-** @brief Point d'entree du programme push_swap
+** @brief Point d’entrée du programme push_swap
 ** @param argc Nombre d'arguments (incluant le nom du programme)
-** @param argv Tableau de chaines des arguments
-** @return int 0 si succes, 1 si erreur
+** @param argv Tableau de chaînes des arguments
+** @return int 0 si succès, 1 si erreur
 ** @see arg_parser() pour le parsing des arguments
-** @warning Les arguments doivent etre des entiers valides
+** @warning Les arguments doivent être des entiers valides
 ** @example ./push_swap 3 2 1
 */
 int	main(int argc, char **argv)
 {
 	ft_printf("Arguments = %i \n", argc - 1);
-	arg_parser(argc, **argv);
+	arg_parser(argc, argv);
 }

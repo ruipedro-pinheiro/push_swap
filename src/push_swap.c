b426138974468e96104f5b1f40714e63fa1d1702
ruipedro-pinheiro <pedroyverdon@gmail.com>
2025-12-08 13:17:47 +0100

    full repo sync from multiple devices

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,17 +6,18 @@
 /*   By: rpinheir <rpinheir@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 14:59:18 by rpinheir          #+#    #+#             */
-/*   Updated: 2025/12/04 19:49:24 by rpinheir         ###   ########.fr       */
+/*   Updated: 2025/12/05 00:56:33 by rpinheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../include/push_swap.h"
 
 /*
 ** @brief Parse les arguments de la ligne de commande
 ** @param argc Nombre d'arguments passes au programme
 ** @param argv Tableau de chaines contenant les arguments
-** @return int (a implementer: code de retour)
-** @note `argc` inclut le nom du programme (argv[0]),
-** donc argc - 1 = nombre d'arguments reels
+** @return t_stack* Pointeur vers le premier node de stack_a (ou NULL si erreur)
+** @note `argc` inclut le nom du programme (argv[0])
 */
 int	arg_parser(int argc, char **argv)
 {
@@ -38,16 +39,13 @@ int	arg_parser(int argc, char **argv)
 }
 
 /*
-** @brief Point d'entree du programme push_swap
+** @brief Point d’entrée du programme push_swap
 ** @param argc Nombre d'arguments (incluant le nom du programme)
-** @param argv Tableau de chaines des arguments
-** @return int 0 si succes, 1 si erreur
-** @see arg_parser() pour le parsing des arguments
-** @warning Les arguments doivent etre des entiers valides
-** @example ./push_swap 3 2 1
+** @param argv Tableau de chaînes des arguments
+** @return int 0 si succès, 1 si erreur
 */
 int	main(int argc, char **argv)
 {
 	ft_printf("Arguments = %i \n", argc - 1);
-	arg_parser(argc, **argv);
+	arg_parser(argc, argv);
 }

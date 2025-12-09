3e9df99994068a7960c419779e23d5f2ac0bc2ff
ruipedro-pinheiro <pedroyverdon@gmail.com>
2025-12-09 12:23:42 +0100

    Adds `sa` function to swap top two elements of stack A.
    Implements the `sa` (swap a) operation, which reorders the first two nodes of a stack. This function correctly updates `next` and `prev` pointers to reflect the swap, providing a core operation for the push_swap algorithm.

--- src/push_swap_operations.c
+++ src/push_swap_operations.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   push_swap_operations.c                             :+:      :+:    :+:   */
@@ -6,9 +6,24 @@
 /*   By: rpinheir <rpinheir@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 17:26:35 by rpinheir          #+#    #+#             */
-/*   Updated: 2025/11/26 17:26:56 by rpinheir         ###   ########.fr       */
+/*   Updated: 2025/12/09 12:19:10 by rpinheir         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "../include/push_swap.h"
 
+
+void	sa(t_stack **stack)
+{
+	t_stack	*first_node;
+	t_stack	*second_node;
+	t_stack	*ptr;
+
+	first_node = *stack;
+	second_node = first_node->next;
+	ptr = first_node;
+	first_node->next = second_node->next;
+	first_node->prev = second_node;
+	second_node->next = ptr;
+	second_node->prev = NULL;
+}

65f41545d7a7f8020c6bdd0fd39fa2556628f13e
Pedro <pedroyverdon@gmail.com>
2025-12-17 11:13:30 +0100

    Debugging

--- src/turk_a_to_b.c
+++ src/turk_a_to_b.c
@@ -6,7 +6,7 @@
 /*   By: rpinheir <rpinhier@student.42Lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 14:19:59 by rpinheir          #+#    #+#             */
-/*   Updated: 2025/12/16 16:16:38 by rpinheir         ###   ########.ch       */
+/*   Updated: 2025/12/16 19:43:32 by rpinheir         ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@ void	get_target_b(t_stack *node_a, t_stack **b)
 {
 	t_stack	*target;
 	t_stack	*node;
-	int		min;
+	t_stack	*min;
 
 	node = *b;
+	min = node;
 	while (1)
 	{
-		if (node == *b)
-			break ;
 		if (node->value > node_a->value && node->value < min)
 			target = node;
+		if (node == *b)
+			break ;
 	}
 	node_a->target_node = target;
 }
--- src/turk_utils.c
+++ src/turk_utils.c
@@ -6,7 +6,7 @@
 /*   By: rpinheir <rpinhier@student.42Lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 20:27:18 by rpinheir          #+#    #+#             */
-/*   Updated: 2025/12/16 14:17:49 by rpinheir         ###   ########.ch       */
+/*   Updated: 2025/12/16 19:58:57 by rpinheir         ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,9 @@ int	is_sorted(t_stack *stack)
 	node = first_node;
 	while (node->next != first_node)
 	{
-		if (node->prev->value < node->value)
-			continue ;
-		else if (node->prev->value > node->value)
+		if (node->next->value < node->value)
 			return (0);
+		node = node->next;
 	}
 	return (1);
 }
@@ -42,11 +41,11 @@ t_stack	*find_min(t_stack *stack)
 	smaller = first_node;
 	while (1)
 	{
-		if (node == stack)
-			break ;
 		if (node->value < smaller->value)
 			smaller = node;
 		node = node->next;
+		if (node == stack)
+			break ;
 	}
 	return (smaller);
 }
@@ -62,11 +61,11 @@ t_stack	*find_max(t_stack *stack)
 	biggest = node;
 	while (1)
 	{
-		if (node == stack)
-			break ;
 		if (node->value > biggest->value)
 			biggest = node;
 		node = node->next;
+		if (node == stack)
+			break ;
 	}
 	return (biggest);
 }
@@ -80,8 +79,9 @@ void	set_median(t_stack *stack)
 	median = ft_listcount(&stack) / 2;
 	while (1)
 	{
+		node->above_median = (node->position < median);
+		node = node->next;
 		if (node == stack)
 			break ;
-		node->above_median = (node->position < median);
 	}
 }
